4977d57592303c95e3fc2fdf4815fcb899d070e1
spekary <spekary@gmail.com>
2019-03-19 18:31:37 +0100

    Fixing build

--- test/ci/test.sh
+++ test/ci/test.sh
@@ -2,11 +2,11 @@
 
 # This script is an aid to running the browser test on travis. It is designed to be run from the goradd-test directory.
 echo "*** building main"
-go build main
+go build goradd-test
 echo "*** start and stop"
-./main -e # sanity check
+./goradd-test -e # sanity check
 echo "*** starting server"
-./main &
+./goradd-test &
 sleep 5
 google-chrome-stable --headless --remote-debugging-port=9222 http://localhost:8000/test?all=1 &
 #"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --remote-debugging-port=9222 http://localhost:8000/test?all=1 &
