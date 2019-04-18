ee98afae71f68c2f6997fd4e1b6f9aac47ca6992
spekary <spekary@gmail.com>
2019-04-18 09:01:15 +0200

    Removing old files

--- internal/install/goradd-project/codegen/cmd/build.go
+++ internal/install/goradd-project/codegen/cmd/build.go
@@ -1,12 +1,21 @@
 package main
 
 // This file executes the complete codegen process by:
-// 1) Removing old template files
+// 1) Removing old files
 // 2) Generating new template files from the template source
 // 3) Building and then running the codegen app
 
-// Generate the templates
+// Remove old files
 //go:generate gofile remove goradd-tmp/template/*.tpl.go
+//go:generate gofile remove goradd-project/gen/*/connector/*.base.go
+//go:generate gofile remove goradd-project/gen/*/form/*.go
+//go:generate gofile remove goradd-project/gen/*/form/template_source/*.tpl.got
+//go:generate gofile remove goradd-project/gen/*/model/*.base.go
+//go:generate gofile remove goradd-project/gen/*/model/node/*.go
+//go:generate gofile remove goradd-project/gen/*/panel/*.base.go
+//go:generate gofile remove goradd-project/gen/*/panel/template_source/inactive/*
+
+// Generate the templates
 //go:generate got -t got -o goradd-tmp/template -I goradd-project/codegen/templates/orm -d github.com/goradd/goradd/codegen/templates/orm
 //go:generate got -t got -o goradd-tmp/template -I goradd-project/codegen/templates/page -d github.com/goradd/goradd/codegen/templates/page
 
