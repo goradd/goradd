d0b1fbce9243c836055bcb6ce98a44d26dea59c8
spekary <spekary@gmail.com>
2019-04-30 22:01:47 +0200

    Working through auth details.

--- pkg/auth/config.go
+++ pkg/auth/config.go
@@ -3,8 +3,8 @@ package auth
 import "time"
 
 // ApiEndpoint is the url for the authorization api. Feel free to change the default during startup.
-var ApiEndpoint = "/auth"
+var ApiEndpoint = "/api/auth"
 // HackerDelay is the amount of time to delay a response if we detect a hacker. Feel free to change the default during startup.
 var HackerDelay = 20  * time.Second
 // Number of seconds to permit between login attempts. Attempts to login faster than this will be rejected.
-var LoginRateLimit int64 = 2
+var LoginRateLimit int64 = 3
