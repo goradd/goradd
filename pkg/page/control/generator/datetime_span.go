a77d8d9652cd78b245540d62370eca145d18dab6
spekary <spekary@gmail.com>
2019-07-24 11:47:21 -0700

    DateTimeSpan connector

--- pkg/page/control/generator/datetime_span.go
+++ pkg/page/control/generator/datetime_span.go
@@ -2,16 +2,13 @@ package generator
 
 import (
 	"fmt"
-	"github.com/goradd/gengen/pkg/maps"
 	"github.com/goradd/goradd/codegen/generator"
 	"github.com/goradd/goradd/pkg/config"
-	"github.com/goradd/goradd/pkg/page"
-	"github.com/goradd/goradd/pkg/page/control"
 )
 
 func init() {
 	if !config.Release {
-		//generator.RegisterControlGenerator(DateTimeSpan{})
+		generator.RegisterControlGenerator(DateTimeSpan{})
 	}
 }
 
@@ -35,36 +32,24 @@ func (d DateTimeSpan) SupportsColumn(col *generator.ColumnType) bool {
 	return true
 }
 
-func (d DateTimeSpan) GenerateCreate(namespace string, col *generator.ColumnType) (s string) {
+func (d DateTimeSpan) GenerateCreator(col *generator.ColumnType) (s string) {
 	s = fmt.Sprintf(
-		`	ctrl = %s.NewDateTimeSpan(c.ParentControl, id)
-	ctrl.SetLabel(ctrl.T("%s"))
-`, namespace, col.DefaultLabel)
-
-
+`control.DateTimeSpanCreator{
+	ID:        %#v,
+	ControlOptions: page.ControlOptions{
+		Disabled:	   %#v,
+		DataConnector: %s{},
+	},
+}`, col.ControlID, col.IsPk, col.Connector)
 	return
 }
 
-func (d DateTimeSpan) GenerateGet(ctrlName string, objName string, col *generator.ColumnType) (s string) {
-	s = fmt.Sprintf(`c.%s.SetDateTime(c.%s.%s())`, ctrlName, objName, col.GoName)
-	return
+
+func (d DateTimeSpan) GenerateRefresh(col *generator.ColumnType) (s string) {
+	return `ctrl.SetDateTime(val)`
 }
 
-func (d DateTimeSpan) GeneratePut(ctrlName string, objName string, col *generator.ColumnType) (s string) {
-	return
+func (d DateTimeSpan) GenerateUpdate(col *generator.ColumnType) (s string) {
+	return ""
 }
 
-func (d DateTimeSpan) ConnectorParams() *maps.SliceMap {
-	paramControls := page.ControlConnectorParams()
-	paramSet := maps.NewSliceMap()
-	paramSet.Set("Format", generator.ConnectorParam{
-		"Format",
-		"format string to use to format the DateTime. See time.Time doc for more info.",
-		generator.ControlTypeString,
-		`{{var}}.SetFormat{{val}}`,
-		func(c page.ControlI, val interface{}) {
-			c.(*control.DateTimeSpan).SetFormat(val.(string))
-		}})
-
-	return paramControls
-}
