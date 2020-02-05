015c5cf7a08bd7fb2920d4e8b5061434e5bce21d
spekary <spekary@gmail.com>
2020-02-04 20:54:18 -0800

    Improving EditBase panel

--- internal/install/goradd-project/codegen/cmd/build.go
+++ internal/install/goradd-project/codegen/cmd/build.go
@@ -16,8 +16,8 @@ package main
 //go:generate gofile remove goradd-project/gen/*/panel/inactive_templates/*
 
 // Generate the templates
-//go:generate got -t got -o goradd-project/tmp/template -I goradd-project/codegen/templates/orm -d github.com/goradd/goradd/codegen/templates/orm
-//go:generate got -t got -o goradd-project/tmp/template -I goradd-project/codegen/templates/page -d github.com/goradd/goradd/codegen/templates/page
+//go:generate got -t got -o goradd-project/tmp/template -I goradd-project/codegen/templates/orm -d github.com/goradd/goradd/codegen/templates/orm -i
+//go:generate got -t got -o goradd-project/tmp/template -I goradd-project/codegen/templates/page -d github.com/goradd/goradd/codegen/templates/page -i
 
 // Run the code generator
 //go:generate go run codegen.go
