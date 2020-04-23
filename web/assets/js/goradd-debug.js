4010239fa290e6a9f7df85d0b7703e7645e012d1
spekary <spekary@gmail.com>
2020-04-23 16:23:47 -0700

    Making sure ajax errors are only displayed in the debug version.

--- web/assets/js/goradd-debug.js
+++ web/assets/js/goradd-debug.js
@@ -1,10 +1,12 @@
 /*
 
-Goradd Testing Additions
+Goradd Debugging Additions
 
-This file attaches some code used by the test harness to drive browser-based tests. It is only loaded in debug mode.
+This file attaches some code used by the test harness to drive browser-based tests, and other
+debug-only code. It is only loaded in debug mode.
 
 */
+
 (function() {
 
     goradd.initFormTest = function() {
@@ -60,6 +62,38 @@ This file attaches some code used by the test harness to drive browser-based tes
             window.opener.postMessage({testMarker: marker}, "*");
         }
     };
+
+    /**
+     * Displays the ajax error in either a popup window, or a new web page.
+     * @param {string} resultText
+     * @param {number} err
+     * @private
+     */
+    goradd.displayAjaxError = function(resultText, err) {
+        var objErrorWindow;
+
+        _ajaxError = true;
+        _blockEvents = false;
+
+        if (resultText.substr(0, 15) === "<!DOCTYPE html>") {
+            window.alert("An error occurred.\r\n\r\nThe error response will appear in a new popup.");
+            objErrorWindow = window.open("about:blank", "qcubed_error", "menubar=no,toolbar=no,location=no,status=no,scrollbars=yes,resizable=yes,width=1000,height=700,left=50,top=50");
+            objErrorWindow.focus();
+            objErrorWindow.document.write(resultText);
+        } else {
+            if (err) {
+                resultText = err.toString();
+                if (err.sourceURL) {
+                    resultText += " File:" + err.sourceURL
+                }
+                if (err.line) {
+                    resultText += " Line:" + err.line;
+                }
+            }
+            var el = goradd.tagBuilder("div").attr("id", "Goradd_AJAX_Error").html("<button onclick=\"window.goradd.g('Goradd_AJAX_Error').remove()\">OK</button>").appendTo(goradd.form());
+            goradd.tagBuilder("div").html(resultText).appendTo(el);
+        }
+    }
 
 
 }) ();
