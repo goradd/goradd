a2dc9f91e18533ebb2f0a2c8218a55cb31351d34
spekary <spekary@gmail.com>
2022-02-02 22:30:40 -0800

    Linux build

--- internal/install/goradd-project/build/embed/buildLinux.go
+++ internal/install/goradd-project/build/embed/buildLinux.go
@@ -1,4 +1,4 @@
 package embed
 
-//go:generate env GOOS=linux GOARCH=amd64 go build -tags "release nodebug" -ldflags "-s -w" -o ../../../deploy/docker/grapp -v ../../main
+//go:generate env GOOS=linux GOARCH=amd64 go build -tags "release nodebug" -ldflags "-s -w" -o ../../deploy/embed -v ../../main
 
