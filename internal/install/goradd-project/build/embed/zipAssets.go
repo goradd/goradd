a68463da907d6af7d77248273ca689c2fe2c6754
spekary <spekary@gmail.com>
2022-02-02 23:00:19 -0800

    Fix build to remove non-zipped files

--- internal/install/goradd-project/build/embed/zipAssets.go
+++ internal/install/goradd-project/build/embed/zipAssets.go
@@ -4,7 +4,7 @@ package embed
 //go:generate minify -r --sync -o ../../deploy/embed/ ../../deploy/stage/root
 
 //go:generate gofile brotli -v -x *.go ../../deploy/embed/
-//go:generate gofile gzip -v -x *.go:*.br ../../deploy/embed/
+//go:generate gofile gzip -v -d -x *.go:*.br ../../deploy/embed/
 
 
 
