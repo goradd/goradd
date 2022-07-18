993486b7fb574d23b0d6555e830fe380189caa01
spekary <spekary@gmail.com>
2022-07-17 22:46:15 -0700

    Fix for database

--- internal/ci/db/goradd_unit.mysql.sql
+++ internal/ci/db/goradd_unit.mysql.sql
@@ -1,121 +1,105 @@
-SET
-FOREIGN_KEY_CHECKS=0;
-SET
-SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET
-time_zone = "+00:00";
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE `double_index`
-(
-    `id`          int(11) NOT NULL,
-    `fieldInt`    int(11) NOT NULL,
-    `fieldString` varchar(50) NOT NULL
+CREATE TABLE `double_index` (
+                                `id` int(11) NOT NULL,
+                                `fieldInt` int(11) NOT NULL,
+                                `fieldString` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_cascade`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) DEFAULT NULL
+CREATE TABLE `forward_cascade` (
+                                   `id` int(11) NOT NULL,
+                                   `name` varchar(100) NOT NULL,
+                                   `reverse_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_cascade_unique`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) DEFAULT NULL
+CREATE TABLE `forward_cascade_unique` (
+                                          `id` int(11) NOT NULL,
+                                          `name` varchar(100) NOT NULL,
+                                          `reverse_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_null`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) DEFAULT NULL
+CREATE TABLE `forward_null` (
+                                `id` int(11) NOT NULL,
+                                `name` varchar(100) NOT NULL,
+                                `reverse_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_null_unique`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) DEFAULT NULL
+CREATE TABLE `forward_null_unique` (
+                                       `id` int(11) NOT NULL,
+                                       `name` varchar(100) NOT NULL,
+                                       `reverse_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_restrict`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) NOT NULL
+CREATE TABLE `forward_restrict` (
+                                    `id` int(11) NOT NULL,
+                                    `name` varchar(100) NOT NULL,
+                                    `reverse_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `forward_restrict_unique`
-(
-    `id`         int(11) NOT NULL,
-    `name`       varchar(100) NOT NULL,
-    `reverse_id` int(11) DEFAULT NULL
+CREATE TABLE `forward_restrict_unique` (
+                                           `id` int(11) NOT NULL,
+                                           `name` varchar(100) NOT NULL,
+                                           `reverse_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `reverse`
-(
-    `id`   int(11) NOT NULL,
-    `name` varchar(100) NOT NULL
+CREATE TABLE `reverse` (
+                           `id` int(11) NOT NULL,
+                           `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `two_key`
-(
-    `server`    varchar(50) NOT NULL,
-    `directory` varchar(50) NOT NULL,
-    `file_name` varchar(50) NOT NULL
+CREATE TABLE `two_key` (
+                           `server` varchar(50) NOT NULL,
+                           `directory` varchar(50) NOT NULL,
+                           `file_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `two_key` (`server`, `directory`, `file_name`)
-VALUES (''cnn.com'', ''us'', ''news''),
-       (''google.com'', ''drive'', ''''),
-       (''google.com'', ''mail'', ''mail.html''),
-       (''google.com'', ''news'', ''news.php''),
-       (''mail.google.com'', ''mail'', ''inbox''),
-       (''yahoo.com'', '''', '''');
+INSERT INTO `two_key` (`server`, `directory`, `file_name`) VALUES
+                                                               ('cnn.com', 'us', 'news'),
+                                                               ('google.com', 'drive', ''),
+                                                               ('google.com', 'mail', 'mail.html'),
+                                                               ('google.com', 'news', 'news.php'),
+                                                               ('mail.google.com', 'mail', 'inbox'),
+                                                               ('yahoo.com', '', '');
 
-CREATE TABLE `type_test`
-(
-    `id`           int(11) NOT NULL,
-    `date`         date        DEFAULT NULL,
-    `time`         time        DEFAULT NULL,
-    `date_time`    datetime    DEFAULT NULL,
-    `ts`           timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `test_int`     int(11) DEFAULT '' 5 '',
-    `test_float`   float       DEFAULT NULL,
-    `test_double`  double NOT NULL,
-    `test_text`    text,
-    `test_bit`     tinyint(1) DEFAULT NULL,
-    `test_varchar` varchar(10) DEFAULT NULL,
-    `test_blob`    blob   NOT NULL
+CREATE TABLE `type_test` (
+                             `id` int(11) NOT NULL,
+                             `date` date DEFAULT NULL,
+                             `time` time DEFAULT NULL,
+                             `date_time` datetime DEFAULT NULL,
+                             `ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                             `test_int` int(11) DEFAULT '5',
+                             `test_float` float DEFAULT NULL,
+                             `test_double` double NOT NULL,
+                             `test_text` text,
+                             `test_bit` tinyint(1) DEFAULT NULL,
+                             `test_varchar` varchar(10) DEFAULT NULL,
+                             `test_blob` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `type_test` (`id`, `date`, `time`, `date_time`, `ts`, `test_int`, `test_float`, `test_double`, `test_text`,
-                         `test_bit`, `test_varchar`, `test_blob`)
-VALUES (1, ''2019 - 01 - 02 '', ''06:17:28'', ''2019 - 01 - 02 06:17:28'', ''2019 - 01 - 23 08:52:06'', 5, 1.2, 3.33,
-        ''Sample'', 1, ''Sample'', 0x61626364);
+INSERT INTO `type_test` (`id`, `date`, `time`, `date_time`, `ts`, `test_int`, `test_float`, `test_double`, `test_text`, `test_bit`, `test_varchar`, `test_blob`) VALUES
+    (1, '2019-01-02', '06:17:28', '2019-01-02 06:17:28', '2019-01-23 08:52:06', 5, 1.2, 3.33, 'Sample', 1, 'Sample', 0x61626364);
 
-CREATE TABLE `unsupported_types`
-(
-    `type_set` set('' a '','' b'','' c '') NOT NULL,
-    `type_enum`        enum('' a '','' b'','' c '') NOT NULL,
-    `type_decimal`     decimal(10, 4) NOT NULL,
-    `type_double`      double         NOT NULL,
-    `type_geo`         geometry       NOT NULL,
-    `type_tiny_blob`   tinyblob       NOT NULL,
-    `type_medium_blob` mediumblob     NOT NULL,
-    `type_varbinary`   varbinary(10) NOT NULL,
-    `type_longtext`    longtext       NOT NULL,
-    `type_binary`      binary(10) NOT NULL,
-    `type_small`       smallint(6) NOT NULL,
-    `type_medium`      mediumint(9) NOT NULL,
-    `type_big`         bigint(20) NOT NULL,
-    `type_polygon`     polygon        NOT NULL,
-    `type_serial`      bigint(20) UNSIGNED NOT NULL,
-    `type_unsigned`    int(10) UNSIGNED NOT NULL,
-    `type_multFk1`     varchar(50)    NOT NULL,
-    `type_multiFk2`    varchar(50)    NOT NULL
+CREATE TABLE `unsupported_types` (
+                                     `type_set` set('a','b','c') NOT NULL,
+                                     `type_enum` enum('a','b','c') NOT NULL,
+                                     `type_decimal` decimal(10,4) NOT NULL,
+                                     `type_double` double NOT NULL,
+                                     `type_geo` geometry NOT NULL,
+                                     `type_tiny_blob` tinyblob NOT NULL,
+                                     `type_medium_blob` mediumblob NOT NULL,
+                                     `type_varbinary` varbinary(10) NOT NULL,
+                                     `type_longtext` longtext NOT NULL,
+                                     `type_binary` binary(10) NOT NULL,
+                                     `type_small` smallint(6) NOT NULL,
+                                     `type_medium` mediumint(9) NOT NULL,
+                                     `type_big` bigint(20) NOT NULL,
+                                     `type_polygon` polygon NOT NULL,
+                                     `type_serial` bigint(20) UNSIGNED NOT NULL,
+                                     `type_unsigned` int(10) UNSIGNED NOT NULL,
+                                     `type_multFk1` varchar(50) NOT NULL,
+                                     `type_multiFk2` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -151,7 +135,7 @@ ALTER TABLE `reverse`
     ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `two_key`
-    ADD PRIMARY KEY (`server`, `directory`);
+    ADD PRIMARY KEY (`server`,`directory`);
 
 ALTER TABLE `type_test`
     ADD PRIMARY KEY (`id`);
@@ -162,28 +146,28 @@ ALTER TABLE `unsupported_types`
 
 
 ALTER TABLE `forward_cascade`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `forward_cascade_unique`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 ALTER TABLE `forward_null`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 ALTER TABLE `forward_null_unique`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 ALTER TABLE `forward_restrict`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 ALTER TABLE `forward_restrict_unique`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 ALTER TABLE `reverse`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 ALTER TABLE `type_test`
-    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 ALTER TABLE `unsupported_types`
     MODIFY `type_serial` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
@@ -206,5 +190,4 @@ ALTER TABLE `forward_restrict`
 
 ALTER TABLE `forward_restrict_unique`
     ADD CONSTRAINT `forward_restrict_unique_ibfk_1` FOREIGN KEY (`reverse_id`) REFERENCES `reverse` (`id`);
-SET
-FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS=1;
